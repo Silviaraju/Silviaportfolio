0915ee12c468640d350dee27f4001dc39f6a6bfe
Silviaraju <47835293+Silviaraju@users.noreply.github.com>
2020-10-20 18:10:58 -0500

    deploy the app to react pages

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Silvia Raju Marihal",
+  "role": "Software Developer",
+  "linkedinId":"https://www.linkedin.com/in/silviaraju ",
+  "skypeid": "http://skype.com",
+  "roleDescription": "I like dabbling in various parts of software development and like to learn about new technologies.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/silviaraju",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/Silviaraju",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"instagram",
+        "url":"https://www.instagram.com/silviaraju/",
+        "className":"fa fa-instagram"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      },
+      {
+        "name":"facebook",
+        "url":"https://www.facebook.com/profile.php?id=100004559746556",
+        "className":"fa fa-facebook"
+      }
+    ],
+  "aboutme":"Aspiring Software/Web/Full-stack Developer with 2 years of experience building scalable web applications using HTML, CSS, JavaScript, Bootstrap, NodeJS, ReactJS and automated application level code to interact with backend. Expertized in Software Development Life Cycle (SDLC) including Software Architecture, Object Oriented Programming, Designing and Prototyping.",
+  "address":"San Diego, California",
+  "website":"https://github.com/Silviaraju",
+  "education":[
+    {
+      "UniversityName":"Wichita State University",
+      "specialization":"Master's in Computer Science",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2020",
+      "Achievements":"My major area of Master's study was related to the designing and developing software systems using scientfic analysis and mathematical models to predict and measure outcomes and design consequences."
+    },
+    {
+      "UniversityName":"Visvesvaraya Technological University",
+      "specialization":"Bachelor's in Information Science and Engineering",
+      "MonthOfPassing":"July",
+      "YearOfPassing":"2017",
+      "Achievements":"My academic field was concerned with the study of theoretical and practical foundations of information processing and computations of practical problems and its solutions."
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Wichita State Uiversity",
+      "specialization":"Graduate Teaching Assistant",
+      "Joining":"June 2019 - ",
+      "Leaving":"May 2020",
+      "Achievements":"Improved laboratory research and suggested modifications to existing curriculum for Object Oriented Programming Concepts, delivered one-to-one assistance to students during practical sessions and workshops to improve their programming capabilities and validated program error messages by finding and correcting bugs in the program."
+    },
+    {
+      "CompanyName":"LifeOfGirl",
+      "specialization":"Software Developer",
+      "Joining":"September 2017 - ",
+      "Leaving":"July 2018",
+      "Achievements":"Developed a new frontend feature on schedule using ReactJS that allows users to easily activate instant security which allows them to find nearest safe place and also utilize its services, designed visual elements to improve User Interface (UI), improved integration of internal and external systems using Spring Integration, designed RESTful backend server enabling user data to be stored persistently in an online database, improved the page speed by 15% for LifeOfGirl website that generates $15K annually in revenue by leveraging browser caching and improving server response times, utilized GraphQL to create APIs for both internal and external use reducing the time to ship new front-end features by 5% and built a unit testing infrastructure using selenium for a client web application that reduced the number of bugs by the client by 8% month over month."
+    },
+    {
+      "CompanyName":"ICICI Bank",
+      "specialization":"Software Developer Intern",
+      "Joining":"June 2016 - ",
+      "Leaving":"August 2017",
+      "Achievements":"Designed customer facing web-app using JavaScript which improved the time on page for an average user by 2 minutes, responsible for writing complex SQL queries for data import and export as required, implemented core banking operations like Employee Information, Departments, Resource Utilization, Project Management, represented information using tables, charts, graphs, etc and also, worked with containerization technologies like Docker & Docker Enterprise, worked within an agile team and helped prioritize and scope feature requests to ensure that the biggest impact features were worked on first and built extensive test coverage for all new features which reduced the number of customer complaints by 9%."
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"ReactJS"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Android: Smart Mobile-Based Emergency Management & Notification Systems",
+      "description":"Emergency App: https://github.com/Silviaraju/Android",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"Robotics: Grasping Skills2",
+      "description":"https://github.com/Silviaraju/Robotics",
+      "imgurl":"images/portfolio/robot.jpg"
+    },
+    {
+      "name":"Cybersecurity",
+      "description":"Sniffing & Spoofing: https://github.com/Silviaraju/Cyber-Security ",
+      "imgurl":"images/portfolio/project2.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Silvia has a zest for life and has a variety of interests. Academics has never been the only thing she managed at a time. She gave herself astutely to her hobbies, academics, team activities, event organization, etc. and maintained a good balance with all of them. This clearly talks about her immaculate time management skills.",
+      "name":"Dr. T John Peter, Head of Department, Sambhram Institute of Technology"
+    },
+    {
+      "description":"Silvia had an excellent rapport with the team members and earned their confidence by showing immaculate reverence. I find her to be a very quick learner, with brilliant analytical abilities; her assignments reflected an extremely high degree of professionalism. I must also add that she has appreciable skills in presentation. She is an excellent orator and can take constructive criticism very well.",
+      "name":"Vignan Joseph, CEO, LifeOfGirl"
+    }
+  ]
+}
+
+export default resumeData
